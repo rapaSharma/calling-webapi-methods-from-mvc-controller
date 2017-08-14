f9d6cf01e3433be506d9e321c26b45bb496339d9
rapaSharma <30756004+rapaSharma@users.noreply.github.com>
2017-08-14 20:48:22 +0530

    Add files via upload

---
+++ CallingWebApiMethodsFromMVCController/MVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using MVC.Models;
+using Newtonsoft.Json;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Hosted web API REST Service base url  
+        private readonly string Baseurl = "http://localhost/WebApi";
+
+        public async Task<ActionResult> Index()
+        {
+            var empInfo = new List<Employee>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var postResponse =
+                    await
+                        client.PostAsJsonAsync(@"/WebApi/api/employee/Add",
+                            new Employee{City = "Chennai", Id = 4, Name = "Vengat"});
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                var res = await client.GetAsync(@"/WebApi/api/employee/GetEmployees");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var empResponse = res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    empInfo = JsonConvert.DeserializeObject<List<Employee>>(empResponse);
+                }
+
+                //returning the employee list to view  
+                return View(empInfo);
+            }
+        }
+    }
+}
---
+++ CallingWebApiMethodsFromMVCController/WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/employee")]
+    public class EmployeeController : ApiController
+    {
+        private static readonly List<Employee> Employees = new List<Employee>
+        {
+            new Employee
+            {
+                City = "Tumakuru",
+                Id = 1,
+                Name = "Sharma"
+            },
+            new Employee
+            {
+                City = "Bengaluru",
+                Id = 2,
+                Name = "Vijay"
+            },
+            new Employee
+            {
+                City = "Hyderabad",
+                Id = 3,
+                Name = "Uma Maheshwar"
+            }
+        };
+
+        [HttpGet]
+        [Route("GetEmployees")]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public void Add([FromBody] Employee employee)
+        {
+            Employees.Add(employee);
+        }
+    }
+}
